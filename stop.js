38110045e5502ba5ea54cf07c0ab05bc7130daa8
Kimmo Rundelin <kiheru@users.sourceforge.net>
2017-12-09 20:43:11 +0200

    Initial commit

---
+++ stop.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function handleClick() {
+	let tab = browser.tabs.query({active: true});
+        browser.tabs.executeScript(tab.id, {
+                code: "window.stop();",
+		allFrames: true,
+		runAt: "document_start"
+        });
+}
+
+function updateState(info) {
+	if ("loading" === info.status) {
+		browser.browserAction.enable();
+	} else {
+		browser.browserAction.disable();
+	}
+}
+
+function tabChanged(activeInfo) {
+	let id = activeInfo.tabId;
+	browser.tabs.get(id).then((tab) => {
+		updateState(tab);
+	});
+}
+
+function tabUpdated(tabId, changeInfo, tab) {
+	if (changeInfo.status) {
+		updateState(changeInfo);
+	}
+}
+
+browser.browserAction.onClicked.addListener(handleClick);
+browser.tabs.onActivated.addListener(tabChanged);
+browser.tabs.onUpdated.addListener(tabUpdated);
